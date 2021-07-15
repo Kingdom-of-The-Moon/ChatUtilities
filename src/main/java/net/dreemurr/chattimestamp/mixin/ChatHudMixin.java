f06c4b58313d883416f82d8893ec8ddc91a0fd2b
Francy-chan <40085587+Francy-chan@users.noreply.github.com>
2021-07-15 08:12:04 -0300

    added a anti-spam :3

--- src/main/java/net/dreemurr/chattimestamp/mixin/ChatHudMixin.java
+++ src/main/java/net/dreemurr/chattimestamp/mixin/ChatHudMixin.java
@@ -4,6 +4,8 @@ import net.dreemurr.chattimestamp.ChatTimeStamp;
 import net.minecraft.client.gui.hud.ChatHud;
 import net.minecraft.client.gui.hud.ChatHudLine;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.OrderedText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Final;
@@ -14,33 +16,108 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.time.LocalTime;
+import java.util.Deque;
 import java.util.List;
 
 @Mixin(ChatHud.class)
-public class ChatHudMixin {
+public abstract class ChatHudMixin {
 
+    private int spam = 1;
+    private int queueSize = 0;
+
+    @Shadow @Final private Deque<Text> messageQueue;
     @Shadow @Final private List<ChatHudLine<Text>> messages;
+    @Shadow @Final private List<ChatHudLine<OrderedText>> visibleMessages;
 
-    @Inject(at = @At("HEAD"), method = "addMessage(Lnet/minecraft/text/Text;)V")
+    @Inject(at = @At("HEAD"), method = "addMessage(Lnet/minecraft/text/Text;)V", cancellable = true)
     public void addMessage(Text message, CallbackInfo ci) {
+        /////////////
+        //anti spam//
+        /////////////
+
+        if (messageQueue.size() >= queueSize && chattimestamp$antiSpam(message, false)) {
+            ci.cancel();
+            return;
+        }
+
+        /////////////
+        //timestamp//
+        /////////////
+
+        //get current time
         LocalTime timeNow = LocalTime.now();
 
-        int hour   = timeNow.getHour() % 12;
+        //format time
+        int hour = timeNow.getHour();
+        hour -= hour >= 13 ? 12 : 0;
         int minute = timeNow.getMinute();
         //int second = timeNow.getSecond();
 
-        boolean newTime = hour != ChatTimeStamp.lastHour || minute != ChatTimeStamp.lastMinute /* || second != ChatTimeStamp.lastSecond */ || this.messages.size() == 0;
+        //define if should send time message
+        boolean newTime = /* second != ChatTimeStamp.lastSecond || */ minute != ChatTimeStamp.lastMinute || hour != ChatTimeStamp.lastHour || messages.isEmpty();
 
         if (newTime) {
-            ChatTimeStamp.lastHour   = hour;
+            //save current time
+            ChatTimeStamp.lastHour = hour;
             ChatTimeStamp.lastMinute = minute;
             //ChatTimeStamp.lastSecond = second;
-            Text time = new LiteralText("[" + String.format("%02d", hour) + ":" + String.format("%02d", minute) /* + ":" + String.format("%02d", second) */ + "]").formatted(Formatting.GRAY);
 
-            this.addMessage(new LiteralText("").append(ChatTimeStamp.style).append(" ").append(time).append(" ").append(ChatTimeStamp.style), 0);
+            //add time message
+            Text time = new LiteralText(" [" + String.format("%02d", hour) + ":" + String.format("%02d", minute) + /* ":" + String.format("%02d", second) + */ "] ").formatted(Formatting.GRAY);
+            this.addMessage(new LiteralText("").append(ChatTimeStamp.style).append(time).append(ChatTimeStamp.style), 0);
         }
     }
 
-    @Shadow
-    private void addMessage(Text message, int messageId) {}
+    @Inject(at = @At("HEAD"), method = "queueMessage", cancellable = true)
+    public void queueMessage(Text message, CallbackInfo ci) {
+        //anti spam
+        if (chattimestamp$antiSpam(message, !this.messageQueue.isEmpty()))
+            ci.cancel();
+
+        //save size
+        queueSize = this.messageQueue.size();
+    }
+
+    @Shadow protected abstract void addMessage(Text message, int messageId);
+
+    //anti spam
+    public boolean chattimestamp$antiSpam(Text message, boolean isQueue) {
+        //get messages
+        String messageString = message.getString();
+        Text lastMessage = null;
+
+        if (isQueue) //get from queue
+            lastMessage = this.messageQueue.getLast();
+        else if (!this.messages.isEmpty()) //get from message list
+            lastMessage = this.messages.get(0).getText();
+
+        //add spam count to the current message if theres spam
+        messageString += spam > 1 ? " x" + spam : "";
+
+        //compare messages
+        if (lastMessage != null && messageString.equals(lastMessage.getString())) {
+            //increase spam
+            spam++;
+
+            //edit old message
+            Text formatted = (((MutableText) message).append(new LiteralText(" x" + spam).formatted(Formatting.DARK_GRAY, Formatting.ITALIC)));
+
+            if (isQueue) {
+                this.messageQueue.removeLast();
+                this.messageQueue.addLast(formatted);
+            } else {
+                ChatHudLine<Text> lastChatMessage = this.messages.get(0);
+                this.messages.set(0, new ChatHudLine<>(lastChatMessage.getCreationTick(), formatted, lastChatMessage.getId()));
+                this.visibleMessages.set(0, new ChatHudLine<>(lastChatMessage.getCreationTick(), formatted.asOrderedText(), lastChatMessage.getId()));
+            }
+
+            //return
+            return true;
+        }
+
+        //reset count if not spam
+        spam = 1;
+
+        return false;
+    }
 }
