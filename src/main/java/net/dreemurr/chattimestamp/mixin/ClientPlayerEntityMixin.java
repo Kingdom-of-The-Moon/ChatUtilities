0a9390f6345675e9d522809c5757e654b474cb25
Francy-chan <40085587+Francy-chan@users.noreply.github.com>
2021-08-15 04:22:15 -0300

    enable chat while in nether portal

---
+++ src/main/java/net/dreemurr/chattimestamp/mixin/ClientPlayerEntityMixin.java
@@ -0,0 +1,23 @@
+package net.dreemurr.chattimestamp.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ChatScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.network.ClientPlayerEntity;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ClientPlayerEntity.class)
+public class ClientPlayerEntityMixin {
+
+    @Shadow @Final protected MinecraftClient client;
+
+    @Redirect(method = "updateNausea", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;setScreen(Lnet/minecraft/client/gui/screen/Screen;)V"))
+    public void updateNausea(MinecraftClient minecraftClient, Screen screen) {
+        if (!(this.client.currentScreen instanceof ChatScreen))
+            this.client.setScreen(null);
+    }
+}
